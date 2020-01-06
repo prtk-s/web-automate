b34b63e226abd0b2fb5549fa167844643473b58d
parteek <parteek.singh@daffodilsw.com>
2020-01-06 14:59:06 +0530

    introduce new binder function with bug fixes

--- utility/error/errorUtil.js
+++ utility/error/errorUtil.js
@@ -1,7 +1,16 @@
-export function undefinedFieldError(field, customMessage = null) {
-  return new Error(customMessage || `${field} is undefined`);
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+exports.undefinedFieldError = undefinedFieldError;
+exports.rejectError = rejectError;
+
+function undefinedFieldError(field) {
+  var customMessage = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+  return new Error(customMessage || ''.concat(field, ' is undefined'));
 }
 
-export function rejectError(reject, error) {
+function rejectError(reject, error) {
   return reject(error);
 }
--- utility/index.js
+++ utility/index.js
@@ -1,2 +1,33 @@
-export { PATH_TYPE, WEBDRIVER_CONSTANT } from './constant';
-export { undefinedFieldError, rejectError } from './error/errorUtil';
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "PATH_TYPE", {
+  enumerable: true,
+  get: function get() {
+    return _constant.PATH_TYPE;
+  }
+});
+Object.defineProperty(exports, "WEBDRIVER_CONSTANT", {
+  enumerable: true,
+  get: function get() {
+    return _constant.WEBDRIVER_CONSTANT;
+  }
+});
+Object.defineProperty(exports, "undefinedFieldError", {
+  enumerable: true,
+  get: function get() {
+    return _errorUtil.undefinedFieldError;
+  }
+});
+Object.defineProperty(exports, "rejectError", {
+  enumerable: true,
+  get: function get() {
+    return _errorUtil.rejectError;
+  }
+});
+
+var _constant = require("./constant");
+
+var _errorUtil = require("./error/errorUtil");
